7e9496e9ba07faed380b5edf9d8b4ba60a3e9451
Gabriel-boop-deep <72049404+Gabriel-boop-deep@users.noreply.github.com>
2022-01-26 10:21:36 -0300

    Update CadastrodeLivrosusandopoo.cs

--- CadastrodeLivrosusandopoo.cs
+++ CadastrodeLivrosusandopoo.cs
@@ -22,24 +22,16 @@
   }
 }
 
-2 aba
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace projeto_api
-{
   public class Livro 
   {
     public string Titulo { get; set; }
     public string Genero { get; set; }
     public int Ano { get; set; }
-    public int Edicao { get; set; }
+    public int Edicao { get; set; 
     
     
   }
-  public Livro(string Titulo,string Genero,int Ano,int anosDeUso)
+  public Livro(string Titulo,string Genero,int Ano,int anosDeUs
   {
     this.Titulo=Titulo;
     this.Genero=Genero;
@@ -56,69 +48,6 @@ namespace projeto_api
 
   
 } 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace projeto_api
-{
-  public class Livro 
-  {
-    public string Titulo { get; set; }
-    public string Genero { get; set; }
-    public int Ano { get; set; }
-    public int Edicao { get; set; }
     
     
-  }
-  public Livro(string Titulo,string Genero,int Ano,int anosDeUso)
-  {
-    this.Titulo=Titulo;
-    this.Genero=Genero;
-    this.Ano=Ano;
-    this.anosDeUso=0;
-  }
-  public override string ToString()
-  {
-    return "O titulo é: "+ this.Titulo +"\n"
-    + "Genero: "+this.Genero+"\n"
-    + "Ano: "+this.Ano+"\n"
-    +"Anos de uso: "+this.anosDeUso;
-  }
-
-  
-}
- using System;
- using System.Collections.Generic;
- using System.Linq;
- using System.Threading.Tasks;
-
- namespace projeto_api
- {
-  public class Livro 
-  {
-   public string Titulo { get; set; }
-   public string Genero { get; set; }
-    public int Ano { get; set; }
-    public int Edicao { get; set; }
-    
-    
-  }
-  public Livro(string Titulo,string Genero,int Ano,int anosDeUso)
-  {
-    this.Titulo=Titulo;
-    this.Genero=Genero;
-    this.Ano=Ano;
-    this.anosDeUso=0;
-  }
-  public override string ToString()
-  {
-    return "O titulo é: "+ this.Titulo +"\n"
-    + "Genero: "+this.Genero+"\n"
-    + "Ano: "+this.Ano+"\n"
-    +"Anos de uso: "+this.anosDeUso;
- }
-
-  
-} 
